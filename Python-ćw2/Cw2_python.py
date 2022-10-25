f95152bc1070a1d499ffedd1a065641a248fd65a
Jarosław <jarek.such0@gmail.com>
2022-10-25 19:43:25 +0200

    Poprawiono zadania z zestawu drugiego.

--- Python-ćw2/Cw2_python.py
+++ Python-ćw2/Cw2_python.py
@@ -2,28 +2,31 @@
 # Zadania 2.10 - 2.19 
 
 # cw. 2.10
-# Mamy dany napis wielowierszowy line. Podaæ sposób obliczenia liczby wyrazów w napisie.
-# Przez wyraz rozumiemy ci¹g "czarnych" znaków,
-# oddzielony od innych wyrazów bia³ymi znakami (spacja, tabulacja, newline).
+# Mamy dany napis wielowierszowy line. Podac sposob obliczenia liczby wyrazow w napisie.
+# Przez wyraz rozumiemy ciag "czarnych" znakow,
+# oddzielony od innych wyrazow bialymi znakami (spacja, tabulacja, newline).
 
 def _2_10(line):   
     l = line.split()
     return len(l)
 
 # cw. 2.11
-# Podaæ sposób wyœwietlania napisu word tak,
-# aby jego znaki by³y rozdzielone znakiem podkreœlenia.
+# Podac sposob wyswietlania napisu word tak,
+# aby jego znaki byly rozdzielone znakiem podkreslenia.
 
 def _2_11():
     word = "harbinger"
     new_ = ""
-    for i in word:
-        new_ = new_ + i + "_"
+    for i in range(len(word)):
+        if i == len(word)-1:
+            new_ += word[i]
+            continue
+        new_ = new_ + word[i] + "_"
     print(new_)
 
 # cw. 2.12
-# Zbudowaæ napis stworzony z pierwszych znaków wyrazów z wiersza line.
-# Zbudowaæ napis stworzony z ostatnich znaków wyrazów z wiersza line.
+# Zbudowac napis stworzony z pierwszych znakow wyrazow z wiersza line.
+# Zbudowac napis stworzony z ostatnich znakow wyrazow z wiersza line.
 
 def _2_12(line):
     l = line.split()
@@ -36,8 +39,8 @@ def _2_12(line):
     print("Z ostatnich liter: {}".format(new_line_last))
 
 # cw. 2.13
-# ZnaleŸæ ³¹czn¹ d³ugoœæ wyrazów w napisie line.
-# Wskazówka: mo¿na skorzystaæ z funkcji sum().
+# Znalezc laczna dlugosc wyrazow w napisie line.
+# Wskazowka: mozna skorzystac z funkcji sum().
 
 def _2_13(line):
     l = line.split()
@@ -47,7 +50,7 @@ def _2_13(line):
     print("Laczna dlugosc wyrazow: {}".format(sum))
 
 # cw. 2.14
-# ZnaleŸæ: (a) najd³u¿szy wyraz,(b) d³ugoœæ najd³u¿szego wyrazu w napisie line.
+# Znalezc: (a) najdluzszy wyraz,(b) dlugosc najdluzszego wyrazu w napisie line.
 
 def _2_14(line):
     l = line.split()
@@ -61,8 +64,8 @@ def _2_14(line):
     print("Najdluzszy wyraz: {}, jego dlugosc wynosi: {}".format(new_line, len(new_line)))
 
 # cw. 2.15
-# Na liœcie L znajduj¹ siê liczby ca³kowite dodatnie.
-# Stworzyæ napis bêd¹cy ci¹giem cyfr kolejnych liczb z listy L.
+# Na liscie L znajduja sie liczby calkowite dodatnie.
+# Stworzyc napis bedacy ciagiem cyfr kolejnych liczb z listy L.
 
 def _2_15():
     l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -73,7 +76,7 @@ def _2_15():
     print(line)
 
 # cw. 2.16
-# W tekœcie znajduj¹cym siê w zmiennej line zamieniæ ci¹g znaków "GvR" na "Guido van Rossum".
+# W tekscie znajdujacym sie w zmiennej line zamienic ciag znakow "GvR" na "Guido van Rossum".
 
 def _2_16():
     line = "GvR is a Dutch programmer best known as the creator of the Python. Everyone like GvR."
@@ -81,18 +84,18 @@ def _2_16():
     print(line)
 
 # cw. 2.17
-# Posortowaæ wyrazy z napisu line raz alfabetycznie, a raz pod wzglêdem d³ugoœci.
-# Wskazówka: funkcja wbudowana sorted().
+# Posortowac wyrazy z napisu line raz alfabetycznie, a raz pod wzgledem dlugosci.
+# Wskazowka: funkcja wbudowana sorted().
 
 def _2_17(line):
     new_line = sorted(line.split(), key=str.lower)
-    descendant = sorted(line.split(), key=str.lower, reverse=True)
+    length = sorted(line.split(), key=lambda s: len(s))
     print("Alphabetically:\n{}".format(new_line))
-    print("\nReverse:\n{}".format(descendant))
+    print("\nLength:\n{}".format(length))
 
 # cw. 2.18
-# ZnaleŸæ liczbê cyfr zero w du¿ej liczbie ca³kowitej.
-# Wskazówka: zamieniæ liczbê na napis.
+# Znalezc liczbe cyfr zero w duzej liczbie calkowitej.
+# Wskazowka: zamienic liczbe na napis.
 
 def _2_18():
     number = 2064587065780246030043410
@@ -104,9 +107,9 @@ def _2_18():
     print("Wystapienia cyfry 0: {}".format(count))
 
 # cw. 2.19
-# Na liœcie L mamy liczby jedno-, dwu- i trzycyfrowe dodatnie.
-# Chcemy zbudowaæ napis z trzycyfrowych bloków, gdzie liczby jedno- i dwucyfrowe bêd¹ mia³y blok dope³niony zerami,
-# np. 007, 024. Wskazówka: str.zfill().
+# Na liscie L mamy liczby jedno-, dwu- i trzycyfrowe dodatnie.
+# Chcemy zbudowac napis z trzycyfrowych blokow, gdzie liczby jedno- i dwucyfrowe beda mialy blok dopelniony zerami,
+# np. 007, 024. Wskazowka: str.zfill().
 
 def _2_19():
     l = [385, 2, 5, 67, 7, 999, 135, 23, 61, 8, 111, 0, 12, 821, 780, 15]
