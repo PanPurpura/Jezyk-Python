e770aaf8ac9d6376282a2fe0438b5d2005961d71
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2023-01-16 14:48:49 +0100

    Zamiana match na if-else

--- Projekt/interface.py
+++ Projekt/interface.py
@@ -12,16 +12,15 @@ directed = input("Czy graf jest skierowany? (True lub False): ")
 menu = "\nMenu programu: \n\n"\
     + "1. Pokaz macierz sasiedztwa i listy        2. Liczba wierzcholkow\n" \
     + "3. Liczba krawedzi                         4. Czy graf jest skierowany?\n"\
-    + "5. Dodaj wierzcholek                       6. Czy wierzcholek znajduje sie w grafie?\n" \
-    + "7. Usun wierzcholek                        8. Dodaj krawedz\n"\
-    + "9. Czy krawedz znajduje sie w grafie?      10. Usun krawedz\n"\
-    + "11. Sprawdz wage podanej krawedzi          12. Iterator po wierzcholkach grafu\n"\
-    + "13. Iterator po wierzcholkach sasiednich   14. Iterator po krawedziach wychodzacych\n"\
-    + "15. Iterator po krawedziach przychodzacych 16. Iterator po krawedziach\n"\
-    + "17. Zwroc kopie grafu                      18. Zwroc graf transponowany\n"\
-    + "19. Zwroc dopelnienie grafu                20. Zwroc podgraf indukowany\n"\
-    + "21. DFS                                    22. BFS\n\n"\
-    + "                     21. Zakoncz dzialanie programu"
+    + "5. Czy wierzcholek znajduje sie w grafie?  6. Usun wierzcholek\n" \
+    + "7. Dodaj krawedz                           8. Czy krawedz znajduje sie w grafie?\n"\
+    + "9. Usun krawedz                            10. Sprawdz wage podanej krawedzi\n"\
+    + "11. Iterator po wierzcholkach grafu        12. Iterator po wierzcholkach sasiednich\n"\
+    + "13. Iterator po krawedziach wychodzacych   14. Iterator po krawedziach przychodzacych\n"\
+    + "15. Iterator po krawedziach                16. Zwroc kopie grafu\n"\
+    + "17. Zwroc graf transponowany               18. Zwroc dopelnienie grafu\n"\
+    + "19. Zwroc podgraf indukowany               20. DFS\n"\
+    + "21. BFS                                    22. Zakoncz dzialanie programu\n"
 
 try:
     n = int(size)
@@ -41,228 +40,206 @@ g = Graph(n, d)
 while True:
     print(menu)
     c = input("Wybierz opcje: ")
-    match c:
-        case "1":
-            cls()
-            print(g.show_matrix())
-            print("Wierzcholki: {}".format(g.show_list_of_nodes()))
-            print("Krawedzie: {}".format(g.show_list_of_edges()))
-        case "2":
-            cls()
-            print("Liczba wierzcholkow: {} \n".format(g.v()))
-        case "3":
-            cls()
-            print("Liczba krawedzi: {} \n".format(g.e()))
-        case "4":
-            cls()
-            print("Skierowany? {} \n".format(g.is_directed()))
-        case "5":
-            cls()
+
+    if c == "1":
+        cls()
+        print(g.show_matrix())
+    elif c == "2":
+        cls()
+        print("Liczba wierzcholkow: {} \n".format(g.v()))
+    elif c == "3":
+        cls()
+        print("Liczba krawedzi: {} \n".format(g.e()))
+    elif c == "4":
+        cls()
+        print("Skierowany? {} \n".format(g.is_directed()))
+    elif c == "5":
+        cls()
+        try:
+            n = int(input("Podaj numer wierzcholka ktory chcesz sprawdzic [0, {}]: ".format(g.n-1)))
+        except ValueError:
+            print("Bledne dane, przekierowanie do menu")
+            continue
+        
+        print(g.has_node(n))
+        print("\n")
+    elif c == "6":
+         cls()
+         try:
+            n = int(input("Podaj numer wierzcholka ktory chcesz usunac [0, {}]: ".format(g.n-1)))
+         except ValueError:
+            print("Bledne dane, przekierowanie do menu")
+            continue
+         
+         g.del_node(n)
+         print("Usunieto polaczenia z wierzcholkiem o numerze: {} \n".format(n))
+    elif c == "7":
+        cls()
+        try:
+            s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
+            t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
+            w = int(input("Podaj wage krawedzi: "))
+        except ValueError:
+            print("Bledne dane, przekierowanie do menu")
+            continue
+        ed = Edge(s, t, w)
+        g.add_edge(ed)
+        print("Dodano krawedz: {}".format(ed))
+    elif c == "8":
+        cls()
+        try:
+            s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
+            t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
+            w = int(input("Podaj wage krawedzi: "))
+        except ValueError:
+            print("Bledne dane, przekierowanie do menu")
+            continue
+        ed = Edge(s,t,w)
+        print(g.has_edge(ed))
+    elif c == "9":
+        cls()
+        try:
+            s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
+            t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
+            w = int(input("Podaj wage krawedzi: "))
+        except ValueError:
+            print("Bledne dane, przekierowanie do menu")
+            continue
+        ed = Edge(s,t,w)
+        g.del_edge(ed)
+        print("Usunieto krawedz: ({}, {}, {})".format(s,t,w))
+    elif c == "10":
+        cls()
+        try:
+            s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
+            t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
+        except ValueError:
+            print("Bledne dane, przekierowanie do menu")
+            continue
+        ed = Edge(s, t)
+        print("Waga podanej krawedzi to: {}".format(g.weight(ed)))
+    elif c == "11":
+        cls()
+        it = g.iternodes()
+        print("Wierzcholek: {}".format(next(it)))
+        while True: 
             try:
-                n = int(input("Podaj numer wierzcholka ktory chcesz dodac [0, {}]: ".format(g.n-1)))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            g.add_node(n)
-            print("Dodano wierzcholek o numerze: {} \n".format(n))
-        case "6":
-            cls()
-            try:
-                n = int(input("Podaj numer wierzcholka ktory chcesz dodac [0, {}]: ".format(g.n-1)))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            print(g.has_node(n))
-            print("\n")
-        case "7":
-            cls()
-            try:
-                n = int(input("Podaj numer wierzcholka ktory chcesz dodac [0, {}]: ".format(g.n-1)))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            g.del_node(n)
-            print("Usunieto wierzcholek o numerze: {} \n".format(n))
-        case "8":
-            cls()
-            try:
-                s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
-                t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
-                w = int(input("Podaj wage krawedzi: "))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            ed = edge(s, t, w)
-            g.add_edge(ed)
-            #del s,t,w
-            print("Dodano krawedz: {}".format(ed))
-        case "9":
-            cls()
-            try:
-                s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
-                t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
-                w = int(input("Podaj wage krawedzi: "))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            ed = edge(s,t,w)
-            print(g.has_edge(ed))
-        case "10":
-            cls()
-            try:
-                s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
-                t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
-                w = int(input("Podaj wage krawedzi: "))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            ed = edge(s,t,w)
-            g.del_edge(ed)
-            print("Usunieto krawedz: ({}, {}, {})".format(s,t,w))
-        case "11":
-            cls()
-            try:
-                s = int(input("Podaj od jakiego wierzcholka wychodzi krawedz: "))
-                t = int(input("Podaj do jakiego wierzcholka wchodzi krawedz: "))
-            except ValueError:
-                print("Bledne dane, przekierowanie do menu")
-                continue
-            print("Waga podanej krawedzi to: {}".format(g.weight(s,t)))
-        case "12":
-            cls()
-            it = g.iternodes()
-            print("Wierzcholek: {}".format(next(it)))
-            while True: 
-                try:
-                    c = input("Wyswietlic nastepny element? (Tak/Nie): ")
-                    match c:
-                        case "Tak":
-                            print("Wierzcholek: {}".format(next(it)))
-                        case "Nie":
-                            break
-                except StopIteration:
-                    print("Koniec elementow")
+                c1 = input("Wyswietlic nastepny element? (Tak/Nie): ")
+                if c1 == "Tak":
+                    print("Wierzcholek: {}".format(next(it)))
+                elif c1 == "Nie":
                     break
-        case "13":
-            cls()
-            node = int(input("Podaj wierzcholek startowy: "))
-            it = g.iteradjacent(node)
-            #print("Wierzcholek: {}".format(next(it)))
-            while True:       
-                try:
-                    c = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
-                    match c:
-                        case "Tak":
-                            print("Wierzcholek: {}".format(next(it)))
-                        case "Nie":
-                            break
-                except StopIteration:
-                    print("Koniec elementow")
+            except StopIteration:
+                print("Koniec elementow")
+                break
+    elif c == "12":
+        cls()
+        node = int(input("Podaj wierzcholek startowy: "))
+        it = g.iteradjacent(node)
+        #print("Wierzcholek: {}".format(next(it)))
+        while True:       
+            try:
+                c1 = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
+                if c1 == "Tak":
+                    print("Wierzcholek: {}".format(next(it)))
+                elif c1 == "Nie":
                     break
-        case "14":
-            cls()
-            node = int(input("Podaj wierzcholek startowy: "))
-            it = g.iteroutedges(node)
-            #print("Krawedz: {}".format(next(it)))
-            while True:     
-                try:
-                    c = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
-                    match c:
-                        case "Tak":
-                            print("Wierzcholek: {}".format(next(it)))
-                        case "Nie":
-                            break
-                except StopIteration:
-                    print("Koniec elementow")
+            except StopIteration:
+                print("Koniec elementow")
+                break
+    elif c == "13":
+        cls()
+        node = int(input("Podaj wierzcholek startowy: "))
+        it = g.iteroutedges(node)
+        #print("Krawedz: {}".format(next(it)))
+        while True:     
+            try:
+                c1 = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
+                if c1 == "Tak":
+                    print("Wierzcholek: {}".format(next(it)))
+                elif c1 == "Nie":
                     break
-        case "15":
-            cls()
-            node = int(input("Podaj wierzcholek startowy: "))
-            it = g.iterinedges(node)
-            #print("Krawedz: {}".format(next(it)))
-            while True:
-                try:
-                    c = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
-                    match c:
-                        case "Tak":
-                            print("Wierzcholek: {}".format(next(it)))
-                        case "Nie":
-                            break
-                except StopIteration:
-                    print("Koniec elementow")
+            except StopIteration:
+                print("Koniec elementow")
+                break
+    elif c == "14":
+        cls()
+        node = int(input("Podaj wierzcholek startowy: "))
+        it = g.iterinedges(node)
+        #print("Krawedz: {}".format(next(it)))
+        while True:
+            try:
+                c1 = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
+                if c1 == "Tak":
+                    print("Wierzcholek: {}".format(next(it)))
+                elif c1 == "Nie":
                     break
-        case "16":
-            cls()
-            it = g.iteredges()
-            #print("Krawedz: {}".format(next(it)))
-            while True:
-                try:
-                    c = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
-                    match c:
-                        case "Tak":
-                            print("Wierzcholek: {}".format(next(it)))
-                        case "Nie":
+            except StopIteration:
+                print("Koniec elementow")
+                break
+    elif c == "15":
+        cls()
+        it = g.iteredges()
+        #print("Krawedz: {}".format(next(it)))
+        while True:
+            try:
+                c1 = input("Witaj w menu iteratora! Wyswietlic nastepny element? (Tak/Nie): ")
+                if c1 == "Tak":
+                    print("Wierzcholek: {}".format(next(it)))
+                elif c1 == "Nie":
                             break
-                except StopIteration:
-                    print("Koniec elementow")
-                    break
-        case "17":
-            cls()
-            g1 = g.copy()
-            print(g1.show_matrix())
-            print("Wierzcholki: {}".format(g1.show_list_of_nodes()))
-            print("Krawedzie: {}".format(g1.show_list_of_edges()))
-        case "18":
-            cls()
-            g1 = g.transpose()
-            print(g1.show_matrix())
-            print("Wierzcholki: {}".format(g1.show_list_of_nodes()))
-            print("Krawedzie: {}".format(g1.show_list_of_edges()))
-        case "19":
-            cls()
-            g1 = g.complement()
-            print(g1.show_matrix())
-            print("Wierzcholki: {}".format(g1.show_list_of_nodes()))
-            print("Krawedzie: {}".format(g1.show_list_of_edges()))
-        case "20":
-            cls()
-            nodes = []
-            while True:
-                c = (input("Podaj wierzcholki dla podgrafu indukowanego (stop == koniec): "))
-                if c == "stop":
-                    break;
-                try:
-                    nodes.append(int(c))
-                except ValueError:
-                    print("Podano bledne dane, obliczanie podgrafu dla podanych wierzcholkow.")
-                    break;
+            except StopIteration:
+                print("Koniec elementow")
+                break
+    elif c == "16":
+        cls()
+        g1 = g.copy()
+        print(g1.show_matrix())
+    elif c == "17":
+        cls()
+        g1 = g.transpose()
+        print(g1.show_matrix())
+    elif c == "18":
+        cls()
+        g1 = g.complement()
+        print(g1.show_matrix())
+    elif c == "19":
+        cls()
+        nodes = []
+        while True:
+            c1 = (input("Podaj wierzcholki dla podgrafu indukowanego (stop == koniec): "))
+            if c1 == "stop":
+                break;
+            try:
+                nodes.append(int(c1))
+            except ValueError:
+                print("Podano bledne dane, obliczanie podgrafu dla podanych wierzcholkow.")
+                break;
                     
-            g1 = g.subgraph(nodes)
-            print(g1.show_matrix())
-            print("Wierzcholki: {}".format(g1.show_list_of_nodes()))
-            print("Krawedzie: {}".format(g1.show_list_of_edges()))
-        case "21":
-            cls()
-            vis = []
-            try:
-                node = int(input("Podaj wierzcholek startowy: "))
-            except ValueError:
-                print("Podano bledne dane, przekierowanie do menu")
-                continue
-            g.DFS(node, vis)
-            print(vis)
-        case "22":
-            cls()
-            vis = []
-            queue = []
-            try:
-                node = int(input("Podaj wierzcholek startowy: "))
-            except ValueError:
-                print("Podano bledne dane, przekierowanie do menu")
-                continue
-            g.BFS(node, vis, queue)
-            print(vis)
-        case "23":
-            cls()
-            print("Konczenie dzialania programu ...")
-            sys.exit()
+        g1 = g.subgraph(nodes)
+        print(g1.show_matrix())
+    elif c == "20":
+        cls()
+        vis = []
+        try:
+            node = int(input("Podaj wierzcholek startowy: "))
+        except ValueError:
+            print("Podano bledne dane, przekierowanie do menu")
+            continue
+        g.DFS(node, vis)
+        print(vis)
+    elif c == "21":
+        cls()
+        vis = []
+        queue = []
+        try:
+            node = int(input("Podaj wierzcholek startowy: "))
+        except ValueError:
+            print("Podano bledne dane, przekierowanie do menu")
+            continue
+        g.BFS(node, vis, queue)
+        print(vis)
+    elif c == "22":
+        cls()
+        print("Konczenie dzialania programu ...")
+        sys.exit()
